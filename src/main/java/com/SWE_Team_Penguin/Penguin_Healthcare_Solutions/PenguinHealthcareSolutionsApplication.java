8677b22769495906878576ed6ff670830aa44963
Dylan Dulmaine <ddulmai1@students.towson.edu>
2025-10-29 03:39:47 -0400

    Created the basic structure for a Springboot Application

---
+++ src/main/java/com/SWE_Team_Penguin/Penguin_Healthcare_Solutions/PenguinHealthcareSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.SWE_Team_Penguin.Penguin_Healthcare_Solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenguinHealthcareSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenguinHealthcareSolutionsApplication.class, args);
+	}
+
+}
